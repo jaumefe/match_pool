043f7359db3b2c74a05102c0256b32f908deed42
jaumefe <jaumef.escrig96@gmail.com>
2025-07-09 19:31:29 +0200

    frontend new views created

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  msg: String
+})
+
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function Login() {
+  router.push('/login')
+}
+
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+    <div class="card">
+        <p>
+            This is the home page of the Match Pool application.
+        </p>
+        <p>
+            <button @click="Login">Login</button>
+        </p>
+    </div>
+</template>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+
+<template>
+  <h2>Login Page</h2>
+  <p>This is the login view.</p>
+</template>
