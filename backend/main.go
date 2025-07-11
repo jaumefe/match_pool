f5ff0bc31545983fae6b6e50644af3209d774063
jaumefe <jaumef.escrig96@gmail.com>
2025-07-11 21:00:34 +0200

    backend/ cors implementation to allow frontend to communicate with backend

--- backend/main.go
+++ backend/main.go
@@ -5,7 +5,9 @@ import (
 	db "match_pool_back/database"
 	"match_pool_back/routes"
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jessevdk/go-flags"
 )
@@ -28,6 +30,16 @@ func main() {
 	}
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	routes.SetUpRoutes(r)
 
 	port := fmt.Sprintf(":%s", optsConfig.Port)
