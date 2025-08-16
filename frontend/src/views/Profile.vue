7ee2be34648ebfcb00967b160777baa3f3cf0e90
jaumefe <jaumef.escrig96@gmail.com>
2025-08-16 12:31:25 +0200

    frontend minor change on visualization

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -44,10 +44,10 @@ onMounted(async() => {
 
 <template>
     <h2>Perfil de l'usuari</h2>
-    <p>Nom: {{ name }}</p>
-    <p>Canviar nom: <input v-model="formData.name" placeholder="Nou nom d'usuari"></input> <button v-if="newNameOK" @click="changeName">Confirmar canvi de nom</button></p>
-    <p>Canviar contrasenya: <input type="password" v-model="formData.newPass" placeholder="Nova contrasenya"></input></p>
-    <p>Confirmar contrasenya: <input type="password" v-model="formData.newPassConf" placeholder="Nova contrasenya"></input></p>
+    <p><b>Nom: </b>{{ name }}</p>
+    <p><b>Canviar nom: </b><input v-model="formData.name" placeholder="Nou nom d'usuari"></input> <button v-if="newNameOK" @click="changeName">Confirmar canvi de nom</button></p>
+    <p><b>Canviar contrasenya: </b><input type="password" v-model="formData.newPass" placeholder="Nova contrasenya"></input></p>
+    <p><b>Confirmar contrasenya: </b><input type="password" v-model="formData.newPassConf" placeholder="Nova contrasenya"></input></p>
     <p v-if="passConfirm"><button @click="changePass">Confirmar canvi de contrasenya</button></p>
     <p v-if="!newPassOK">La contrasenya no és idèntica</p>
 </template>
